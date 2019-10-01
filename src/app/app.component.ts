56c1d30f12d0da2db5b636956c75cfe18d1fa589
Duong Gia Lac <lacduonggia1998@gmail.com>
2019-10-02 01:01:11 +0700

    Create dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DetailInfoComponent } from './detail-info/detail-info.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SmartHomeAngular';
+
+  constructor(public dialog: MatDialog, ) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DetailInfoComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ChartsComponent } from './charts/charts.component';
+import { DetailInfoComponent } from './detail-info/detail-info.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ChartsModule } from 'ng2-charts';
+import { DialogMonitorButtonComponent } from './dialog-monitor-button/dialog-monitor-button.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartsComponent,
+    DetailInfoComponent,
+    DialogMonitorButtonComponent,
+  ],
+  entryComponents: [
+    DetailInfoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ChartsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
